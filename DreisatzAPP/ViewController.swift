9e1f01348fe9cfeba86fa4f634cc0a84b53bac26
Alexander Thiel <alexthiel@me.com>
2018-03-30 20:13:03 +0200

    Update für Swift 4 und Auto-Layout repariert

--- DreisatzAPP/ViewController.swift
+++ DreisatzAPP/ViewController.swift
@@ -16,13 +16,13 @@ import UIKit
             //Border
             self.layer.cornerRadius = 10.0;
             self.layer.borderWidth = 1.5
-            self.layer.borderColor = UIColor.lightGrayColor().CGColor
+			self.layer.borderColor = UIColor.lightGray.cgColor
             
             //Background
             //self.backgroundColor = UIColor.lightGrayColor()
             //Text
             //self.textColor = UIColor.whiteColor()
-            self.textAlignment = NSTextAlignment.Right
+			self.textAlignment = NSTextAlignment.right
             
         }
     }
@@ -52,7 +52,7 @@ import UIKit
 			Solutionphrase_ini()
         }
         
-        override func canBecomeFirstResponder() -> Bool {
+        func canBecomeFirstResponder() -> Bool {
             return true
             
         }
@@ -62,7 +62,7 @@ import UIKit
             Eingabe2_TF.text = ""
             Eingabe3_TF.text = ""
             Formel_LF.text = ""
-            Ergebnis_Label.text = ""
+            Ergebnis_Label.text = NSLocalizedString("Ergebnis_Label", comment:"Ergebnis_Label")
             //Solution_Label.text = ""
 			Solutionphrase_ini()
         }
@@ -87,34 +87,34 @@ import UIKit
 
 		}
 		
-        override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
             view.endEditing(true)
-            super.touchesBegan(touches, withEvent: event)
+			super.touchesBegan(touches, with: event)
         }
         
-        override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
-            if motion == .MotionShake{
+		override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+			if motion == .motionShake{
                 clearButtonPressed()
             }
         }
         
         
-        func textFieldShouldReturn(textField: UITextField) -> Bool {
+		func textFieldShouldReturn(_ textField: UITextField) -> Bool {
             self.view.endEditing(true);
             return false;
         }
         
         
-        func textFieldShouldClear(textField: UITextField) -> Bool {
+		func textFieldShouldClear(_ textField: UITextField) -> Bool {
             checkEingabe()
 			Solutionphrase()
             return true
         }
         
         
-        func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+		func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
             
-            textField.text = (textField.text! as NSString).stringByReplacingCharactersInRange(range, withString: string)
+			textField.text = (textField.text! as NSString).replacingCharacters(in: range, with: string)
 			Solutionphrase()
 			checkEingabe()
             return false
@@ -133,48 +133,52 @@ import UIKit
 
 
 			let zahl1_hilfe = NSString (string:Eingabe1_TF.text!)
-			let zahl1_formel = zahl1_hilfe.stringByReplacingOccurrencesOfString(",", withString: ".")
+			let zahl1_formel = zahl1_hilfe.replacingOccurrences(of: ",", with: ".")
 			let zahl1 = NSString (string: zahl1_formel).doubleValue
 
-			let zahl2_hilfe = NSString (string:Eingabe2_TF.text!)
-			let zahl2_formel = zahl2_hilfe.stringByReplacingOccurrencesOfString(",", withString: ".")
-			let zahl2 = NSString (string: zahl2_formel).doubleValue
-
-			let zahl3_hilfe = NSString (string:Eingabe3_TF.text!)
-			let zahl3_formel = zahl3_hilfe.stringByReplacingOccurrencesOfString(",", withString: ".")
-			let zahl3 = NSString (string: zahl3_formel).doubleValue
 			
-            
-            if Eingabe1_TF.text!.lengthOfBytesUsingEncoding(NSASCIIStringEncoding) == 0 {
+			let zahl2_hilfe = NSString (string:Eingabe2_TF.text!)
+			let zahl2_formel = zahl2_hilfe.replacingOccurrences(of: ",", with: ".")
+			let zahl2 = NSString (string: zahl2_formel).doubleValue
+			
+			let zahl3_hilfe = NSString (string:Eingabe3_TF.text!)
+			let zahl3_formel = zahl3_hilfe.replacingOccurrences(of: ",", with: ".")
+			let zahl3 = NSString (string: zahl3_formel).doubleValue
+			            
+			if Eingabe1_TF.text!.lengthOfBytes(using: String.Encoding.ascii) == 0 {
                 self.Formel_LF.text = NSLocalizedString("Eingabe_1", comment: "Eingabe 1 ist leer")
                 return
             } else if
                 
-                Eingabe2_TF.text!.lengthOfBytesUsingEncoding(NSASCIIStringEncoding) == 0 {
+				Eingabe2_TF.text!.lengthOfBytes(using: String.Encoding.ascii) == 0 {
                     Formel_LF.text = NSLocalizedString("Eingabe_2", comment: "Eingabe 2 ist leer")
                     return
             } else if
                 
-                Eingabe3_TF.text!.lengthOfBytesUsingEncoding(NSASCIIStringEncoding) == 0 {
+				Eingabe3_TF.text!.lengthOfBytes(using: String.Encoding.ascii) == 0 {
                     Formel_LF.text = NSLocalizedString("Eingabe_3", comment: "Eingabe 3 ist leer")
                     return
             }
             
             
-//          Formel_LF.text = "(\(zahl2) / \(zahl3))  * \(zahl1) = \(zahl2 / zahl3 * zahl1)"
-//          Ergebnis_Label.text = numberFormatter.stringFromNumber(zahl2 / zahl3 * zahl1)
-
-			Formel_LF.text = "(\(zahl2) * \(zahl1))  / \(zahl3) = \(zahl2 * zahl1 / zahl3)"
-			Ergebnis_Label.text = numberFormatter.stringFromNumber(zahl2 * zahl1 / zahl3)
-
+			Ergebnis_Label.text = String(format: "%.2f", locale:Locale.current, (zahl2 * zahl1) / zahl3)
+			Formel_LF.text = "(\(zahl2) * \(zahl1))  / \(zahl3) =  " + Ergebnis_Label.text!
 			
-			Solutionphrase()
 			
+
+				
+				
+//		objCType;: Solutionphrase()
+		Solutionphrase()
 		}
-        
-		var numberFormatter: NSNumberFormatter {
-			let formatter = NSNumberFormatter()
-			formatter.numberStyle = .DecimalStyle
+		
+
+		
+//
+		var numberFormatter: NumberFormatter {
+			let formatter = NumberFormatter()
+			formatter.numberStyle = .decimal
+
 			return formatter
 		}
         
